321aca0aa9f8b7354389aba775f73c0981453511
Vinay Singh <vinaysingh8866@gmail.com>
2022-12-29 08:42:44 -0500

    added new prod

---
+++ prod/_next/static/kawWTJh7nb_6_CnUkxZKs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/790-d675d3624924840b.js","static/chunks/pages/index-90f9bdfe1f5762d2.js"],"/_error":["static/chunks/pages/_error-b5352164729facd9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
